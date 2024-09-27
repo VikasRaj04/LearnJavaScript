3bfec24b563a0885f403d0eb7811e3d48801cd76
Vikas Raj <89587247+VikasRaj04@users.noreply.github.com>
2024-09-27 18:03:29 +0000

    conversion Complete

--- 01_Basics/03_conversionOperation.js
+++ 01_Basics/03_conversionOperation.js
@@ -17,7 +17,7 @@ let valueInNumber = Number(score);
 let isLoggedIn = 1;
 
 let booleanIsLoggedIn = Boolean(isLoggedIn);
-console.log(booleanIsLoggedIn);
+// console.log(booleanIsLoggedIn);
 
 // 1 => true;
 // 0 => false;
@@ -27,5 +27,39 @@ console.log(booleanIsLoggedIn);
 let someNumber = 33;
 let stringSomeNumber = String(someNumber);
 
-console.log(stringSomeNumber);
-console.log(typeof stringSomeNumber);
+// console.log(stringSomeNumber);
+// console.log(typeof stringSomeNumber);
+
+
+
+
+// ============ Operations ==========
+
+
+
+let value = 3;
+let negValue = -value;
+// console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2%2);
+
+let str1 = "hello";
+let str2 = " Vikas";
+let str3 = str1 + str2;
+// console.log(str3);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+
+// console.log(+true);
+
+let gameCounter = 100;
+++gameCounter;
+
+console.log(gameCounter);
