ab0aadbe2a7e62c4e043f06fc2f317300b1506ca
Vikas Raj <89587247+VikasRaj04@users.noreply.github.com>
2024-09-27 17:43:51 +0000

    conversion datatype

---
+++ 01_Basics/03_conversionOperation.js
@@ -0,0 +1,31 @@
+let score = "44abc";
+
+// console.log(typeof score);
+// console.log(typeof(score));
+
+let valueInNumber = Number(score);
+// console.log(typeof(valueInNumber));
+// console.log(valueInNumber);
+
+// "33" => 33;
+// "44abc" => NaN;
+// true => 1;
+// false => 0;
+// Null = 0;
+// Undefined => NaN;
+
+let isLoggedIn = 1;
+
+let booleanIsLoggedIn = Boolean(isLoggedIn);
+console.log(booleanIsLoggedIn);
+
+// 1 => true;
+// 0 => false;
+// " " => false;
+// "Vikas" => true;
+
+let someNumber = 33;
+let stringSomeNumber = String(someNumber);
+
+console.log(stringSomeNumber);
+console.log(typeof stringSomeNumber);
