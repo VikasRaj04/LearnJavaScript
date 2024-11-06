d2816024e8fd44dfa69ce1ab90ffaafec1389a2d
Vikas Raj <89587247+VikasRaj04@users.noreply.github.com>
2024-11-06 18:41:31 +0000

    Prototypes in JS

--- 10_Classes_and_OOP/Prototype.js
+++ 10_Classes_and_OOP/Prototype.js
@@ -1,15 +1,75 @@
-function SetUsername(username){
-    // complex DB calls
-    this.username = username;
-    console.log("called");
+// let myName = "Vikas      ";
+// let channel = "chai      ";
+
+// console.log(myName.trueLength);
+
+
+let myHeros = ["thor", "spiderman"];
+
+
+let heroPower = {
+    thor: "hammer",
+    spider: "sling",
+
+    getSpiderPower: function(){
+        console.log(`Spidy Power is ${this.spider}`);
+    }
 }
 
-function createUser(username, email, password){
-    SetUsername.call(this, username);
-
-    this.email = email;
-    this.password = password;
+Object.prototype.vikas = function(){
+    console.log(`Vikas is present in all objects`);
 }
 
-const chai = new createUser("chai", "chai@fb.com", "123");
-console.log(chai);
+Array.prototype.heyVikas = function(){
+    console.log(`Vikas says hello`);
+}
+
+// heroPower.vikas();
+// myHeros.vikas(); 
+// myHeros.heyVikas();
+// heroPower.heyVikas();
+
+
+
+
+// Inheritence
+
+const User = {
+    name: "chai",
+    email: "chai@google.com"
+}
+
+const Teacher = {
+    makeVideo: true
+}
+
+const TeachingSupport = {
+    isAvailable: false
+}
+
+const TASupport = {
+    makeAssignment: 'JS assignment',
+    fullTime: true,
+    __proto__: TeachingSupport
+}
+
+Teacher.__proto__ = User;
+
+
+// Modern Syntax
+
+Object.setPrototypeOf(TeachingSupport, Teacher);
+
+
+let anotherUsername = "ChaiAurCode       ";
+
+String.prototype.trueLength = function(){
+    console.log(`${this}`);
+    console.log(`True length is ${this.trim().length}`);
+}
+
+anotherUsername.trueLength();
+
+
+"Vikas".trueLength();
+"iceTea".trueLength();
